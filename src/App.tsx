668bcd5281d69e19e78791019abf749ee5f67b2b
JDA <jda@Jordans-MacBook-Air.local>
2024-10-29 14:04:54 -0400

    wardrobe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import NavBar from "./components/NavBar"
+import Wardrobe from './components/Wardrobe'
 
 function App() {
 
   return (
     <>
       <NavBar/>
+      <Routes>
+      <Route path="/wardrobe" element={<Wardrobe />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/Wardrobe.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+function Wardrobe() {
+    const [clothes, setClothes] = useState([])
+
+  
+    return (
+      <>
+       <p>Placeholder for Wardrobe</p>
+      </>
+    )
+
+    }
+
+export default Wardrobe
